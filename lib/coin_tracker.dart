bd49e215a9dc0dac2e76cbf8366e75b7b30ac5d2
x <vinaysharma.2559@gmail.com>
2021-04-30 18:07:30 +0530

    added dropdown

--- lib/coin_tracker.dart
+++ lib/coin_tracker.dart
@@ -8,11 +8,39 @@ class CoinTracker extends StatefulWidget {
 }
 
 class _CoinTrackerState extends State<CoinTracker> {
+  String? _dropdownValue = "INR";
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text("testing..."),
+      child: Column(
+        children: [
+          Expanded(
+            flex: 4,
+            child: Column(
+              children: [],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.blue.shade400,
+              child: Center(
+                child: DropdownButton(
+                    value: _dropdownValue,
+                    onChanged: (String? value) =>
+                        setState(() => _dropdownValue = value),
+                    iconSize: 30.0,
+                    icon: const Icon(
+                      Icons.arrow_upward,
+                      color: Colors.white,
+                    ),
+                    items: <String>["INR", "USD", "JPY", "CAD", "CNY"]
+                        .map((e) => DropdownMenuItem(child: Text(e), value: e))
+                        .toList()),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
