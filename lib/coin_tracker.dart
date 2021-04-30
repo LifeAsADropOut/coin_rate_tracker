93f178ae27845425a7b94b27005bf88561f8fd03
x <vinaysharma.2559@gmail.com>
2021-04-30 17:18:14 +0530

    testing...

---
+++ lib/coin_tracker.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CoinTracker extends StatefulWidget {
+  CoinTracker({Key? key}) : super(key: key);
+
+  @override
+  _CoinTrackerState createState() => _CoinTrackerState();
+}
+
+class _CoinTrackerState extends State<CoinTracker> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("testing..."),
+      ),
+    );
+  }
+}
---
+++ lib/utils.dart
@@ -0,0 +1,7 @@
+const String API_KEY = "EF55E320-AD93-44DD-9F94-6A1B8BB84645";
+
+Uri getURI() => Uri.parse(
+      "https://rest.coinapi.io/v1/exchangerate/BTC/USD",
+    );
+
+// header "X-CoinAPI-Key", "73034021-THIS-IS-SAMPLE-KEY":
