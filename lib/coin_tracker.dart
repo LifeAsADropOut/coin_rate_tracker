67884d20173d7d6311a7fae031415a795045add9
x <vinaysharma.2559@gmail.com>
2021-05-03 16:54:51 +0530

    Future.wait..

--- lib/coin_tracker.dart
+++ lib/coin_tracker.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'package:coin_rate_tracker/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class CoinTracker extends StatefulWidget {
   CoinTracker({Key? key}) : super(key: key);
@@ -9,7 +11,40 @@ class CoinTracker extends StatefulWidget {
 }
 
 class _CoinTrackerState extends State<CoinTracker> {
-  String? _dropdownValue = "INR";
+  String? _dropdownValue = "USD";
+  String? currency = "USD";
+  int? BTC_rate;
+  int? LTC_rate;
+  int? ETH_rate;
+
+  Future getExchangeRate(String e) async {
+    http.Response _res = await http.get(getURI(e, _dropdownValue!), headers: {
+      "X-CoinAPI-Key": API_KEY,
+    });
+
+    if (_res.statusCode == 200) {
+      var _data = jsonDecode(_res.body);
+      print(_data);
+      return _data['rate'];
+    } else
+      print("something went wrong...");
+  }
+
+  Future testingFutureWait(String e) async {
+    var _data = await Future.wait(
+      [
+        getExchangeRate("BTC"),
+        getExchangeRate("LTC"),
+        getExchangeRate("ETH"),
+      ],
+    );
+    // setState(() {
+    //   currency = e;
+    //   BTC_rate = _data[0]!.round();
+    //   LTC_rate = _data[1]!.round();
+    //   ETH_rate = _data[2]!.round();
+    // });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,25 +54,11 @@ class _CoinTrackerState extends State<CoinTracker> {
           Expanded(
             flex: 4,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Padding(
-                  padding: EdgeInsets.fromLTRB(10, 18, 18, 0),
-                  child: Card(
-                    elevation: 5.0,
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(
-                          vertical: 15.0, horizontal: 10.0),
-                      child: Text(
-                        "1 BTC = ? USD",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(color: Colors.white, fontSize: 20.0),
-                      ),
-                    ),
-                    color: Colors.lightBlueAccent,
-                  ),
-                ),
+                getCoinCard(currency!, BTC_rate, "BTC"),
+                getCoinCard(currency!, LTC_rate, "LTC"),
+                getCoinCard(currency!, ETH_rate, "ETH"),
               ],
             ),
           ),
@@ -55,12 +76,35 @@ class _CoinTrackerState extends State<CoinTracker> {
                       color: Colors.white,
                     ),
                     items: CURRENCY_LIST
-                        .map((e) => DropdownMenuItem(child: Text(e), value: e))
+                        .map(
+                          (e) => DropdownMenuItem(
+                              child: Text(e),
+                              value: e,
+                              onTap: () => testingFutureWait(e)),
+                        )
                         .toList()),
               ),
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget getCoinCard(String currency, int? rate, String coin) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(10, 18, 18, 0),
+      child: Card(
+        elevation: 5.0,
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+          child: Text(
+            "1 $coin = ${rate == null ? "?" : rate} $currency",
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white, fontSize: 20.0),
+          ),
+        ),
+        color: Colors.lightBlueAccent,
       ),
     );
   }
--- lib/utils.dart
+++ lib/utils.dart
@@ -1,9 +1,14 @@
 const String API_KEY = "EF55E320-AD93-44DD-9F94-6A1B8BB84645";
 
-Uri getURI() => Uri.parse(
-      "https://rest.coinapi.io/v1/exchangerate/BTC/USD",
+Uri getURI(String coin, String param) => Uri.parse(
+      "https://rest.coinapi.io/v1/exchangerate/$coin/$param",
     );
 
-// header "X-CoinAPI-Key", "73034021-THIS-IS-SAMPLE-KEY":
+// header "X-CoinAPI-Key":"73034021-THIS-IS-SAMPLE-KEY"
 
 const List<String> CURRENCY_LIST = <String>["INR", "USD", "JPY", "CAD", "CNY"];
+const List<String> COIN_LIST = [
+  "BTC",
+  "LTC",
+  "ETH",
+];
